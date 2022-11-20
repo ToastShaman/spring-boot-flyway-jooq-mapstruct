9dc490ff9910b24e20ee7d4d9c52bc297b0577ce
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2022-11-20 21:17:37 +0000

    Record builders

--- src/main/java/com/example/demo/storage/sql/EventRecordMapper.java
+++ src/main/java/com/example/demo/storage/sql/EventRecordMapper.java
@@ -3,21 +3,22 @@ package com.example.demo.storage.sql;
 import static java.time.ZoneOffset.UTC;
 import static org.jooq.generated.Tables.EVENT;
 
+import com.example.demo.events.domain.Event;
+import com.example.demo.events.domain.EventBuilder;
 import com.example.demo.events.domain.EventId;
 import com.example.demo.events.domain.EventStartDate;
 import com.example.demo.events.domain.EventTitle;
 import com.example.demo.events.domain.EventVersion;
-import com.example.demo.events.domain.Event;
 import org.jooq.Record;
 
 public interface EventRecordMapper {
 
   static Event<EventId> map(Record record) {
-    return new Event<>(
-        new EventId(record.get(EVENT.EVENT_ID)),
-        new EventVersion(record.get(EVENT.VERSION)),
-        new EventTitle(record.get(EVENT.TITLE)),
-        new EventStartDate(record.get(EVENT.START_DATE).toInstant(UTC))
-    );
+    return EventBuilder.<EventId>builder()
+        .id(new EventId(record.get(EVENT.EVENT_ID)))
+        .version(new EventVersion(record.get(EVENT.VERSION)))
+        .title(new EventTitle(record.get(EVENT.TITLE)))
+        .startDate(new EventStartDate(record.get(EVENT.START_DATE).toInstant(UTC)))
+        .build();
   }
 }
